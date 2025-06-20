f4295eee511e99adca0cbb0d788bb89e47d34049
kit-lec <kit_lec@hotmail.com>
2025-06-20 21:49:01 +0900

    250620

--- test3_env.py
+++ test3_env.py
@@ -1,4 +1,8 @@
 import os
 print(f'✅ {os.path.basename( __file__ )} 실행됨')  # os.path.absname() 은 전체경로
 
-print('\t', os.getenv('OPEN_API_KEY')[:20])
+from dotenv import load_dotenv
+print(load_dotenv())  # 정상적으로 환경변수 읽어오면 True 리턴.
+
+print('\t', os.getenv('OPENAI_API_KEY')[:20])
+print(f'\tLANGCHAIN_API_KEY={os.getenv('LANGCHAIN_API_KEY')[:20]}...')
